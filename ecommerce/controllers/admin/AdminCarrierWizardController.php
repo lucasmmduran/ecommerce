58aa455bb3263db42fe2764be766961e6716457f
Eiker Gonzalez <eiker.gonzalez21@gmail.com>
2023-02-07 17:34:10 -0400

    test ajuste transportista

--- ecommerce/controllers/admin/AdminCarrierWizardController.php
+++ ecommerce/controllers/admin/AdminCarrierWizardController.php
@@ -72,12 +72,12 @@ class AdminCarrierWizardControllerCore extends AdminController
                 [
                     'title' => $this->trans('General settings', [], 'Admin.Shipping.Feature'),
                 ],
-                [
+                /* [
                     'title' => $this->trans('Shipping locations and costs', [], 'Admin.Shipping.Feature'),
                 ],
                 [
                     'title' => $this->trans('Size, weight, and group access', [], 'Admin.Shipping.Feature'),
-                ],
+                ], */
                 [
                     'title' => $this->trans('Summary', [], 'Admin.Global'),
                 ], ],
@@ -122,9 +122,9 @@ class AdminCarrierWizardControllerCore extends AdminController
             'wizard_contents' => [
                 'contents' => [
                     0 => $this->renderStepOne($carrier),
-                    1 => $this->renderStepThree($carrier),
-                    2 => $this->renderStepFour($carrier),
-                    3 => $this->renderStepFive($carrier),
+                    /* 1 => $this->renderStepThree($carrier),
+                    2 => $this->renderStepFour($carrier), */
+                    1 => $this->renderStepFive($carrier),
                 ],
             ],
             'labels' => [
