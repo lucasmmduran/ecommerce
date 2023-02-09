9e23ae2ace50675a6b108801befa8c047eb41b20
root <root@vilsar.net>
2023-02-09 15:16:35 -0300

    actualizacion de repo

---
+++ ecommerce/modules/ets_multilayerslider/translations/ag.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_f422de11b2c5ee2731049197f3f6282d'] = 'Slider';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_bdcba457bbd5e2c4dee7707751b13130'] = 'Generador visual de presentaciones de diapositivas de la página de inicio de arrastrar y soltar';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_fc029308351f31f8fe047451beb1835b'] = 'FUENTE POR DEFECTO';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_eb9a56428f85bbdfd3846322baaffe6e'] = 'Rebotar';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_bf2c44e6fc09515648b91b1bb4ec3f5f'] = 'Destello';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_ec8374db32bacb4cd9760199ec42819e'] = 'Legumbres';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_2b188c40a02000186fd15c1a56d1273e'] = 'Banda elástica';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_968818e489bac02c58d8b0e5a348a229'] = 'Agitar';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_198b44a40aa77f2256886010c7526c0b'] = 'Balancearse';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_04a5411dd8c9a2cbfacf479dfd20fa21'] = 'Tada';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_5930aa47d87712061d008e75237c7bcb'] = 'Tambalearse';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_bedad9eef4de4b391cc5aeb8ddbe6387'] = 'gelatina';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_a85d1311ec044499e8f666043f0b41da'] = 'Rebote entrada';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_3852d1edb910e440b8bb2fa8cbfe4408'] = 'Rebote abajo';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_83e3baba04bef6b0e685e6433188b3f6'] = 'Rebote izquierda';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_1ba781993dfc04bf49e8a9832eca30f3'] = 'Rebote derecha';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_b1fd46d0d264ef5e7c4486173fb50743'] = 'Rebote arriba';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_9e1a8395c00f7482e1d21b159ce12a4b'] = 'Desvanecer entrada';
+$_MODULE['<{ets_multilayerslider}prestashop>ets_multilayerslider_8ddb0b0158b147ae5d72034ebf73cb51'] = 'Desvanecer hacia abajo';
